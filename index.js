ce76d903a9748711bbbbd8fc9c4ce540415e3a25
Wesley <wes.beck21@outlook.com>
2021-03-03 12:49:04 -0800

    commit tree 27557af0c4f305fc8b7b71629a8a825f479fda7f

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
+  static capitalize( string ) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize( string ) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize( sentence ) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = [];
+    let senWords = sentence.split(" ")
+    for ( let i = 0; i < senWords.length; i++) {
+      if (i == 0) {
+        result.push(this.capitalize(senWords[i]))
+      } else {
+        if( exceptions.includes(senWords[i])){
+          result.push(senWords[i])
+        } else {
+          result.push(this.capitilize(senWords[i]))
+        }
+      }
+    }
+    return result.join(" ");
+  }
 }
